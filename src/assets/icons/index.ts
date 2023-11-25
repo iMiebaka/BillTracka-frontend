30ac553de762861f6db52532e1ba2703cbdb3100
imiebaka <triple07even@gmail.com>
2023-11-25 16:58:22 +0100

    bottom navbar  asserts added

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,12 +2,26 @@ import leftArrow from "./left-arrow.svg";
 import caretDown from "./caret-down.svg";
 import handWave from "./hand-wave.svg";
 import eyeClosed from "./eye-closed.svg";
+import client from "./bottomNavigator/clients.svg";
+import home from "./bottomNavigator/home.svg";
+import task from "./bottomNavigator/task.svg";
+import more from "./bottomNavigator/more.svg";
+import taskFull from "./bottomNavigator/task-fill.svg";
+import clientFill from "./bottomNavigator/clients-fill.svg";
+import homeFill from "./bottomNavigator/home-fill.svg";
 
 const ICONS = {
   leftArrow,
   caretDown,
   handWave,
   eyeClosed,
+  client,
+  home,
+  task,
+  more,
+  taskFull,
+  clientFill,
+  homeFill,
 };
 
 export default ICONS;
